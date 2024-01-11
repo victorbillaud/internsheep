b25ce67a7720ec950f3f343877ff1717bf11a7d6
victorbillaud <victorbillaud@gmail.com>
2024-01-11 18:46:37 +0100

    fix

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -7,10 +7,10 @@ export default function Page() {
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
+                Login into your account
               </h1>
               <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
+                Enter your email below to continue to your account
               </p>
             </div>
             <LoginForm />
