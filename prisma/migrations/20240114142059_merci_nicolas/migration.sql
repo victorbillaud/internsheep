0f9eaa9265fa17ffb7d5a187ea3ea91d18b0894c
nicochanns <nicolas.chalant@efrei.net>
2024-01-14 18:25:09 +0100

    Merge migration / suppression user

--- prisma/migrations/20240114142059_merci_nicolas/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `status` on the `Internship` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Internship" DROP COLUMN "status";
--- prisma/migrations/20240114142839_string_to_int/migration.sql
+++ prisma/migrations/20240114142839_string_to_int/migration.sql
@@ -1,15 +1,17 @@
-/*
-  Warnings:
+-- Drop the existing table if it exists
+DROP TABLE IF EXISTS "Internship";
 
-  - The `remuneration` column on the `Internship` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+-- Create the new table
+CREATE TABLE "Internship" (
+    "id" SERIAL NOT NULL,
+    "companyName" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "mission" TEXT NOT NULL,
+    "numberWeeks" INTEGER NOT NULL,
+    "remuneration" TEXT NOT NULL,
+    "rythm" TEXT NOT NULL,
+    
+    CONSTRAINT "Internship_pkey" PRIMARY KEY ("id")
+);
 
-*/
--- AlterTable
-ALTER TABLE "Internship" ALTER COLUMN "companyName" DROP NOT NULL,
-ALTER COLUMN "startDate" DROP NOT NULL,
-ALTER COLUMN "endDate" DROP NOT NULL,
-ALTER COLUMN "mission" DROP NOT NULL,
-ALTER COLUMN "numberWeeks" DROP NOT NULL,
-DROP COLUMN "remuneration",
-ADD COLUMN     "remuneration" INTEGER,
-ALTER COLUMN "rythm" DROP NOT NULL;
