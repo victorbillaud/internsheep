e4a526dd3461a00350aed9ef5940d9bf4bf4c079
victorbillaud <victorbillaud@gmail.com>
2024-01-22 19:16:47 +0100

    admin

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,18 +1,14 @@
 import { DashboardNav } from "@/components/nav/DashboardNav";
-import { getServerSession } from "next-auth";
 
 export default async function DashboardLayout({children}: {children: React.ReactNode}) {
-  const session = await getServerSession();
-
   return (
     <div className="w-screen h-screen flex flex-col">
       <DashboardNav
         items={[
           {title: "Home", href: "/dashboard"},
-          {title: "Users", href: "/dashboard/users"},
-          {title: "Internships", href: "/dashboard/internships"},
+          {title: "Users", href: "/dashboard/users", adminOnly: true},
+          {title: "Internships", href: "/dashboard/internships"}
         ]}
-        session={session}
       />
       <div className="w-full flex flex-col space-y-5 justify-center items-center mt-20 lg:px-56 md:px-36 sm:px-24 px-10">
         {children}
--- src/components/nav/DashboardNav.tsx
+++ src/components/nav/DashboardNav.tsx
@@ -1,8 +1,9 @@
-import Link from "next/link";
+"use client"
 
 import { cn } from "@/lib/utils";
-import { Session } from "next-auth";
-import { AdminBadge } from "./AdminBadge";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { Badge } from "../ui/badge";
 
 export interface NavItem {
   title: string;
@@ -10,6 +11,7 @@ export interface NavItem {
   disabled?: boolean;
   external?: boolean;
   label?: string;
+  adminOnly?: boolean;
 }
 
 export interface NavItemWithChildren extends NavItem {
@@ -22,19 +24,21 @@ export interface SidebarNavItem extends NavItemWithChildren {}
 
 interface MainNavProps {
   items?: NavItem[];
-  session?: Session | null;
 }
 
-export function DashboardNav({items, session}: MainNavProps) {
+export function DashboardNav({items}: MainNavProps) {
+  const session = useSession();
+  const isAdmin = session?.data?.user?.role === "ADMIN";
+
   return (
     <div className="absolute top-0 left-0 right-0 z-50 flex items-center justify-between py-4 bg-white backdrop-blur-md lg:px-56 md:px-36 sm:px-24 px-10 shadow-sm">
       <Link href="/" className="flex items-center space-x-2">
         <span className="inline-block font-bold">{"Internship"} </span>
-        <AdminBadge />
+        {isAdmin && <Badge variant="destructive">Admin</Badge>}
       </Link>
       {items?.length ? (
         <nav className="flex gap-6">
-          {items?.map(
+          {items.filter(item => !item.adminOnly || isAdmin).map(
             (item, index) =>
               item.href && (
                 <Link
